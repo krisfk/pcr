4631819373549188da916fabe959976697781cf9
kaiyu li <krisfk@gmail.com>
2022-11-14 05:06:43 +0800

    .

--- wp-content/themes/pcr/header.php
+++ wp-content/themes/pcr/header.php
@@ -90,7 +90,10 @@ session_start();
                 <?php
         
         $main_menu = wp_get_menu_array('main menu');
-foreach ($main_menu as $menu_item) {
+        print_r($menu_item);
+
+        foreach ($main_menu as $menu_item) {
+
 
 $url = $menu_item['url'];
 $title = $menu_item['title'];
