75f71eb299a1dca314850df2881ae5dddb80f052
kaiyu li <krisfk@gmail.com>
2022-11-16 01:57:43 +0800

    .

--- traffic/article.php
+++ traffic/article.php
@@ -34,6 +34,24 @@ require('../wp-load.php');
         $post_id=$_GET['aid'];
         ?>
         <h4 class="mt-4">Traffic of <?php echo get_the_title($post_id);?></h4>
+
+        <div>
+
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                    value="option1" />
+                <label class="form-check-label" for="inlineRadio1">1</label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                    value="option2" />
+                <label class="form-check-label" for="inlineRadio2">2</label>
+            </div>
+
+
+
+        </div>
     </div>
 
     <div class="container mt-4">
