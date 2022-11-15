49b1a173befce230fab7ffca99d4590b93175132
kaiyu li <krisfk@gmail.com>
2022-11-15 22:26:53 +0800

    .

--- wp-content/themes/pcr/single.php
+++ wp-content/themes/pcr/single.php
@@ -224,6 +224,40 @@ get_header();
 
                     ?>
 
+        <?php
+if($show_article)
+{
+
+    $servername = "localhost";
+    $username = "pcr_db_user";
+    $password = "eLg5}g@%cNg?8[-v";
+    $dbname = "pcr_traffic";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+echo 'test';
+// $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+// VALUES ('John', 'Doe', 'john@example.com')";
+
+// if ($conn->query($sql) === TRUE) {
+//   echo "New record created successfully";
+// } else {
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+// }
+
+$conn->close();
+
+
+}
+
+?>
+
+
         <div class="article-main-content <?php echo $show_article ? '':'d-none';?>">
 
             <?php 
