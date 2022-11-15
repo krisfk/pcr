ba1b2669791d66711191dca4d97487723862ba6e
kaiyu li <krisfk@gmail.com>
2022-11-16 04:56:26 +0800

    .

--- traffic/article.php
+++ traffic/article.php
@@ -56,7 +56,8 @@
                 var from_date = date_arr[0];
                 var to_date = date_arr[2];
 
-
+                window.location =
+                    '<?php echo 'https://perspectivecr.org/traffic/article.php?aid='.$_GET['aid'].'&from_date='.$from_date.'&to_date='.$to_date;?>';
             })
     })
     </script>
