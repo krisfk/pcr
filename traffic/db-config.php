d79136e5bce7c0a2c1c80770da7098a0b46c0c84
kaiyu li <krisfk@gmail.com>
2022-11-16 02:58:23 +0800

    .

--- traffic/db-config.php
+++ traffic/db-config.php
@@ -13,17 +13,16 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM traffic_record";
+// $sql = "SELECT * FROM traffic_record";
 
-if ($result = $conn -> query($sql)) {
-  while ($row = $conn -> fetch_row()) {
-    echo 1;
-    // printf ("%s (%s)\n", $row[0], $row[1]);
-  }
-  $result -> free_result();
-}
+// if ($result = $conn -> query($sql)) {
+//   while ($row = $conn -> fetch_row()) {
 
-$mysqli -> close();
+// }
+//   $result -> free_result();
+// }
+
+// $mysqli -> close();
 
 
 ?>
