3aa0907a2eb15629a36d7c511306fbe3c7d7a5b1
kaiyu li <krisfk@gmail.com>
2022-11-16 02:42:46 +0800

    .

--- wp-content/themes/pcr/single.php
+++ wp-content/themes/pcr/single.php
@@ -228,7 +228,7 @@ get_header();
 if($show_article)
 {
 
-    require(get_site_url().'/traffic/db-config.php');
+    require('https://perspectivecr.org/traffic/db-config.php');
 
     
 //     $servername = "localhost";
